0ee5bb06020295628c7cab0fb1a2c66addfcc036
Sadra Samadi <samadi_s@live.com>
2019-03-11 21:57:51 +0330

    added browserslist

--- babel.config.js
+++ babel.config.js
@@ -2,47 +2,17 @@ module.exports = function (api) {
 	api.cache(true);
 	return {
 		presets: [
-			[
-				'@babel/preset-env',
-				{
-					useBuiltIns: 'usage'
-				}
-			]
+			['@babel/preset-env', {useBuiltIns: 'usage'}]
 		],
 		plugins: [
-			[
-				'@babel/plugin-transform-runtime'
-			],
-			[
-				'@babel/plugin-proposal-decorators',
-				{
-					legacy: true
-				}
-			],
-			[
-				'@babel/plugin-proposal-class-properties',
-				{
-					loose: true
-				}
-			],
-			[
-				'@babel/plugin-proposal-private-methods',
-				{
-					loose: true
-				}
-			],
-			[
-				'@babel/plugin-proposal-function-bind'
-			],
-			[
-				'@babel/plugin-proposal-optional-chaining'
-			],
-			[
-				'@babel/plugin-proposal-nullish-coalescing-operator'
-			],
-			[
-				'@babel/plugin-proposal-numeric-separator'
-			]
+			['@babel/plugin-transform-runtime'],
+			['@babel/plugin-proposal-decorators', {legacy: true}],
+			['@babel/plugin-proposal-class-properties', {loose: true}],
+			['@babel/plugin-proposal-private-methods', {loose: true}],
+			['@babel/plugin-proposal-function-bind'],
+			['@babel/plugin-proposal-optional-chaining'],
+			['@babel/plugin-proposal-nullish-coalescing-operator'],
+			['@babel/plugin-proposal-numeric-separator']
 		]
 	};
 };
